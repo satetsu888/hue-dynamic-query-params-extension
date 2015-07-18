68da26efa34d210e278fc1c65e244f4b6cf0c4c4
shotaro.akiyama <satetsu888@gmail.com>
2015-07-18 14:12:42 +0900

    fix #2, fire keydown event after assign params

--- src/inject/set_dinamic_query_params.js
+++ src/inject/set_dinamic_query_params.js
@@ -8,7 +8,7 @@ function set(yaml){
             file: "js/jquery.min.js"
         }, function(){
             chrome.tabs.executeScript(null,{
-                code: "$('#execute-parameter-selection form fieldset .control-group').each(function(index, element){var param_name=element.children[0].innerText;;element.children[1].children[0].value=data[param_name];})"
+                code: "$('#execute-parameter-selection form fieldset .control-group').each(function(index, element){var param_name=element.children[0].innerText;var text_input=element.children[1].children[0]; text_input.value=data[param_name];var e=new KeyboardEvent('keydown'); text_input.dispatchEvent(event);})"
             });
         });
     });
